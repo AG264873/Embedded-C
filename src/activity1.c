ae728229f0b4df229c18f86a42d6896538a92686
AG264873 <83201374+AG264873@users.noreply.github.com>
2021-04-27 13:31:04 +0530

    Update activity1.c

--- src/activity1.c
+++ src/activity1.c
@@ -5,11 +5,11 @@
 int fstactivity(void){
     DDRB = DDRB&0b11111101;//fd
     DDRC = DDRC|0b01000000;//40
-    DDRB = DDRB&0b11111110;
+    DDRB = DDRB&0b11111110;//fe
 
 
     while(1){
-        if((PINB & 0b00000010)||(PINB & 0b0000001)==1){//02
+        if((PINB & 0b00000010)||(PINB & 0b0000001)==1){//02 & 01
             PORTC = PORTC&0b10111111;//bf
         }
         else{
