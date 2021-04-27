9daf188c98711f8896e16b23bba946cec2dddb8d
AG264873 <83201374+AG264873@users.noreply.github.com>
2021-04-27 20:09:59 +0530

    Add files via upload

---
+++ inc/activity2.h
@@ -0,0 +1,17 @@
+#include <avr/io.h>
+
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+
+/*******************************************FUNCTION PROTOTYPE**********************************************************/
+//function prototypes to be used
+
+/**
+ * @brief In This activity we to intialize and enable to the ADC and read the channels of the ADC
+ *
+ */
+
+int sndactivity(void);
+
+
+#endif // __FUNCTION_H__
