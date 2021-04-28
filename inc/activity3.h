3e8356fee93635b1d8d608c5d395879440ab5e66
AG264873 <83201374+AG264873@users.noreply.github.com>
2021-04-28 21:41:35 +0530

    Add files via upload

---
+++ inc/activity3.h
@@ -0,0 +1,17 @@
+#include <avr/io.h>
+
+#ifndef __ACTIVITY_THREE_H__
+#define __ACTIVITY_THREE_H__
+
+/*******************************************FUNCTION PROTOTYPE**********************************************************/
+//function prototypes to be used
+
+/**
+ * @brief In this activity we are showing PWM wave for various Duty Cycles on CRO
+ *
+ */
+
+int trdactivity();
+
+
+#endif // __ACTIVITY_THREE_H__
