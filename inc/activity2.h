079541680ce4c3c9aaa1bad87e0df57e78c1cfab
AG264873 <83201374+AG264873@users.noreply.github.com>
2021-04-28 21:04:30 +0530

    Update activity2.h

--- inc/activity2.h
+++ inc/activity2.h
@@ -1,7 +1,7 @@
 #include <avr/io.h>
 
-#ifndef __FUNCTION_H__
-#define __FUNCTION_H__
+#ifndef __ACTIVITY_TWO_H__
+#define __ACTIVITY_TWO_H__
 
 /*******************************************FUNCTION PROTOTYPE**********************************************************/
 //function prototypes to be used
@@ -14,4 +14,4 @@
 int sndactivity(void);
 
 
-#endif // __FUNCTION_H__
+#endif // __ACTIVITY_TWO_H__
