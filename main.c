bf2ead92f6d2648031063c203b231d13668b3836
AG264873 <83201374+AG264873@users.noreply.github.com>
2021-04-29 21:42:43 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
-#include "activity1.h"
-#include "activity2.h"
+//#include "activity1.h"
+//#include "activity2.h"
 #include "activity3.h"
+//#include "activity4.h"
+
+/**
+ * @brief All the activities are integrated into activity three.
+ *
+ */
 
 int main(void){
-    fstactivity();
-    sndactivity();
+    
+    //fstactivity();
+    //sndactivity();
     trdactivity();
+    //frthactivity();
+    
     return 0;
 }
