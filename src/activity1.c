be765af082fc169708e09764abecd75b9b5cbd36
AG264873 <83201374+AG264873@users.noreply.github.com>
2021-04-29 21:29:08 +0530

    Update activity1.c

--- src/activity1.c
+++ src/activity1.c
@@ -6,15 +6,12 @@ int fstactivity(void){
     DDRD = DDRD&0b11111101;//fd
     DDRD = DDRD|0b01000000;//40
     DDRB = DDRB&0b11111110;//fe
-
-
-    while(1){
-        if((PIND & 0b00000010)||(PINB & 0b0000001)==1){//02 & 01
-            PORTD = PORTD&0b10111111;//bf
-        }
-        else{
-            PORTD = PORTD|0b01000000;//40
-        }
+    
+    if((PIND & 0b00000010)||(PINB & 0b0000001)==1){//02 & 01
+        PORTD = PORTD&0b10111111;//bf
+    }
+    else{
+        PORTD = PORTD|0b01000000;//40
     }
 
 }
