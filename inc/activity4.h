9cc7ce0517e0aa2a9e4083a6ce66cde1089a4c86
AG264873 <83201374+AG264873@users.noreply.github.com>
2021-04-29 21:27:35 +0530

    Add files via upload

---
+++ inc/activity4.h
@@ -0,0 +1,17 @@
+#include <avr/io.h>
+
+#ifndef __ACTIVITY_FOUR_H__
+#define __ACTIVITY_FOUR_H__
+
+/*******************************************FUNCTION PROTOTYPE**********************************************************/
+//function prototypes to be used
+
+/**
+ * @brief In this activity we are Displaying different values of temperature on serial monitors
+ *
+ */
+
+int frthactivity();
+
+
+#endif // __ACTIVITY_FOUR_H__
